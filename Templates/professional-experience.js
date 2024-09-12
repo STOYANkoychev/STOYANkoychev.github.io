4145e59b74d9646b64d396c95b9097b434617e10
Stoyan Koychev <music.designed.6@gmail.com>
2024-09-12 22:44:37 +0300

    Update and change Education.css professional-experience.ccs/js fail

--- Templates/professional-experience.js
+++ Templates/professional-experience.js
@@ -1,82 +1,61 @@
 export default function (content) {
   content.innerHTML = `
-       <h2>Professional Experience</h2>
+      <h2 class="title-prof-experience">Professional Experience</h2>
     
     <div class="company" id="fantastico">
-       <div class="company-info">
-       <h3>Fantastico Group OOD</h3>
-       <img src="https://assets.jobs.bg/assets/logo/2022-03-31/s_e6743a0ee573aea1806b1f9d22b09944.jpg" alt="Fantastico Group OOD">
-            <h4>System Administrator</h4>
-            <p>August 2023 - March 2024</p>
-            <ul>
-                <li>Responsible for installing, configuring, and maintaining the hardware and software necessary for the operation of the company's commercial and office systems.</li>
-                <li>Performed continuous monitoring of computer systems and networks to detect and prevent potential issues before they caused disruptions.</li>
+       
+          <h3>Fantastico Group OOD</h3>
+          <img src="https://assets.jobs.bg/assets/logo/2022-03-31/s_e6743a0ee573aea1806b1f9d22b09944.jpg" alt="Fantastico Group OOD">
+          <div class="job-description" id="sys-administrator">
+             <h3>System Administrator</h3>
+             <p>August 2023 - March 2024</p>
+             <ul>
+                <li>Install, configure, and maintain the hardware and software required for the company's business and office systems.</li>
+                <li>Monitor computer systems and networks to detect and prevent potential problems.</li>
                 <li>Provided technical support and assistance to employees, helping to resolve issues with computers, printers, network connections, and other technical devices.</li>
-            </ul>
-        </div>
-    </div>
-    
-    <div class="company" id="accountant">
-      
-        <div class="company-info">
+                <li>Testing new software.</li>
+             </ul>
+          </div> 
+          <div class="job-description" id="accountant">
+
             <h3>Accountant, Invoice Control</h3>
-            <h4>February 2012</h4>
+            <p>February 2012</p>
             <ul>
-                <li>Maintained financial records, processed and prepared transactions, ensuring compliance with accounting standards.</li>
+                <li>Manages financial documentation, verifying and processing invoices to ensure accuracy and adherence to company standards.</li>
                 <li>Controlled invoicing to ensure accuracy and compliance with company deliveries.</li>
-                <li>Accounted for invoices in accordance with applicable accounting principles and internal company policies.</li>
-                <li>Prepared regular reports on financial status, including liabilities to suppliers and current expenses.</li>
-                <li>Communicated with suppliers to resolve any issues or disputes related to invoices, such as missing documents or discrepancies.</li>
-                <li>Worked closely with internal departments like sales, logistics, and legal to ensure proper invoice processing.</li>
+                <li>Monitors payments and resolves discrepancies by coordinating with vendors and customers.</li>
+                <li>Collaborates with other departments to ensure invoice accuracy and assists with audit preparation..</li>
             </ul>
-        </div>
-    </div>
-    
-    <div class="company" id="senior-operator">
-    
-        <div class="company-info">
+         </div>
+
+         <div class="job-description" id="operator">
             <h3>Senior Data Operator</h3>
-            <h4>November 2009</h4>
+            <p>November 2009</p>
             <ul>
                 <li>Managed data entry tasks, ensuring the integrity and accuracy of information.</li>
                 <li>Issued necessary documents for store operations, monitored inventory levels, and managed deliveries.</li>
+                <li>Reported issues to management and assisted in resolving data-related problems</li>
+                <li>Provided training and support to junior staff.</li>
             </ul>
-        </div>
-    </div>
-    
-    <div class="company" id="computer-operator">
-    
-        <div class="company-info">
-            <h3>Computer Operator</h3>
-            <h4>December 2006</h4>
-            <ul>
-                <li>Managed data entry tasks and maintained the computer system for inventory management.</li>
-            </ul>
-        </div>
-    </div>
-    
-    <div class="company" id="technical-assistant">
-   
-        <div class="company-info">
+         </div>
+         <div class="job-description" id="technical-assistant">
             <h3>Technical Assistant</h3>
-            <h4>October 2004</h4>
+            <p>October 2004</p>
             <ul>
-                <li>Provided technical support and ensured the proper functioning of computer systems in the store network.</li>
+                <li>Technical support and ensured the proper proper functioning of the store's computer systems and equipment.</li>
             </ul>
-        </div>
-    </div>
-    
+         </div>
     <div class="company" id="dunev-motor-sport">
-        <img src="" alt="Dunev Motor Sport">
-        <div class="company-info">
-            <h3>Petroleum Products Salesperson</h3>
-            <h4>November 2001 - October 2004</h4>
+        <h3>ET Dunev Motor Sport</h3>
+        <img src="/Images/Dunev logo.png" width="100" height="80" id="dmsImage">
+        <div class="job-description" id="salesman">
+            <h3>Seller of petroleum products</h3>
+            <p>November 2001 - October 2004</p>
             <ul>
                 <li>Managed the sale of petroleum products, maintained customer relations, and ensured product quality.</li>
             </ul>
         </div>
     </div>
     
-</div>
     `;
 }
