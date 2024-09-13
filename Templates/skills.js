f6dac844527214d690be37e2cff371fade4ede67
Stoyan Koychev <music.designed.6@gmail.com>
2024-09-13 11:07:55 +0300

    Create skills.css update skills fail

--- Templates/skills.js
+++ Templates/skills.js
@@ -1,26 +1,40 @@
 export default function (content) {
   content.innerHTML = `
-        <h2>Skills</h2>
+        <h2 class="skills-title" >Skills</h2>
         <div class="skills-box">
             <div class="skill">
-                <span>Accounting</span>
-                <div class="skill-bar"><div class="skill-level" style="width: 90%">Accounting</div></div>
+                
+                <div class="skill-bar"><div class="skill-level" style="width: ">Attention to Detail</div></div>
             </div>
             <div class="skill">
-                <span>QA Testing</span>
-                <div class="skill-bar"><div class="skill-level" style="width: 70%">QA Testing</div></div>
+            
+                <div class="skill-bar"><div class="skill-level" style="width: ">Communication</div></div>
             </div>
             <div class="skill">
-                <span>Communication</span>
-                <div class="skill-bar"><div class="skill-level" style="width: 85%">Communication</div></div>
+             
+                <div class="skill-bar"><div class="skill-level" style="width: ">Problem-Solving</div></div>
             </div>
             <div class="skill">
-                <span>Teamwork</span>
-                <div class="skill-bar"><div class="skill-level" style="width: 80%">Teamwork</div></div>
+              
+                <div class="skill-bar"><div class="skill-level" style="width: ">Teamwork</div></div>
             </div>
             <div class="skill">
-                <span>Finance and Investments</span>
-                <div class="skill-bar"><div class="skill-level" style="width: 75%">Finance and Investments</div></div>
+           
+             <div class="skill">
+              
+                <div class="skill-bar"><div class="skill-level" style="width: ">Critical Thinking</div></div>
+            </div>
+
+                <div class="skill-bar"><div class="skill-level" style="width: ">QA Testing</div></div>
+
+            </div>
+            <div class="skill">
+                
+                <div class="skill-bar"><div class="skill-level" style="width: ">Work to deadlines</div></div>
+            </div>
+             <div class="skill">
+                
+                <div class="skill-bar"><div class="skill-level" style="width: ">Accounting</div></div>
             </div>
         </div>
     `;
