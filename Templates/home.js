2a80f9d3be1d6a918fd891e90c097d1e64d008ec
Stoyan Koychev <music.designed.6@gmail.com>
2024-10-01 23:23:19 +0300

    addFooterHtmlAddFooter.cssAddImagesFooterChangePlaceSocial.links

--- Templates/home.js
+++ Templates/home.js
@@ -2,21 +2,11 @@ export default function (content) {
   content.innerHTML = `
         <div class="home-content">
              <h2>About Me</h2>
-            <p class="text-info-home">I am a highly motivated and detail-oriented individual with a strong background in system administration, accounting, and data management.
+            <p class="text-info-home">I am a highly motivated and detail-oriented professional with a strong background in system administration, accounting, and data management.
              My extensive experience has equipped me with a keen eye for detail, effective problem-solving abilities, and the capacity to respond swiftly and effectively in high-pressure situations.
-             I am currently pursuing a career transition into the IT field, learning the latest testing methodologies and continuously enhancing my skills to deliver reliable and high-performing software solutions.
+             Currently, I am transitioning into the IT field, focusing on learning the latest testing methodologies while continuously enhancing my skills to deliver reliable, high-performing software solutions.
              I enjoy playing the guitar in my free time, and I am also a member of a rock band.</p>
-            <div class="social-icons">
-                <a href="https://www.linkedin.com/in/stoyan-koychev-21382a326/" target="_blank"><i class="fa-brands fa-linkedin fa-3x fa-beat"></i></a>
-                
-                <a href="https://github.com/STOYANkoychev" target="_blank"><i class="fab fa-github fa-3x fa-beat"></i></a>
-
-                <a href="https://www.twitter/" target="_blank"><i class="fab fa-twitter-square fa-3x fa-beat"></i></a>
-
-                <a href="https://www.facebook/" target="_blank"><i class="fab fa-facebook-square fa-3x fa-beat"></i></a>
-               
-
-            </div>
+           
         </div>
     `;
 }
